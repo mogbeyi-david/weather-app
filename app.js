b6c8515c43cb0247c99f03263cc2b967f3fbc07b
David Mogbeyi <mogbeyidavid@gmail.com>
2018-06-29 16:31:31 +0100

    tesing the commit from PHPStorm

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const request = require('request');
+const yargs = require('yargs');
 
+const argv = yargs.options({
+  a: {
+    demand: true,
+    alias: 'address',
+    describe: 'Address to fetch weather for',
+    string: true
+  }
+}).help()
+  .alias('help', 'h')
+  .argv();
+console.log(argv);
 request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
   json: true
 }, (error, response, body) => {
   "use strict";
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}` );
-  console.log(`Latitude: ${body.results[0].geometry.location.lng}` );
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
 });
