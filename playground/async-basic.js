6c128f4a10c5c0cb885be9ab5fefd42cea2ecec5
David Mogbeyi <mogbeyidavid@gmail.com>
2018-06-29 11:42:16 +0100

    Wrapping up asynchronous basics

--- playground/async-basic.js
+++ playground/async-basic.js
@@ -1,6 +1,13 @@
 console.log('starting app...');
+
 setTimeout(() => {
-  "use strict";
+  'use strict';
   console.log('inside the callback');
 } , 4000);
+
+setTimeout(() => {
+  "use strict";
+  console.log('No delay at all');
+} , 0)
+
 console.log('finishing app');
