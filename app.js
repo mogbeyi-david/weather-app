c03385b4734d622fba5f65f2481c394bdf42e862
David Mogbeyi <mogbeyidavid@gmail.com>
2018-07-02 11:46:00 +0100

    Made the Latitudes and the Longitudes dynamic

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
-// const yargs = require('yargs');
-// const geocode = require('./geocode/geocode');
-//
-// const argv = yargs
-//   .options({
-//     a: {
-//       demand: true,
-//       alias: 'address',
-//       describe: 'Address to fetch weather for',
-//       string: true
-//     }
-//   }).help()
-//   .alias('help', 'h')
-//   .argv;
-//
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//   "use strict";
-//   if (errorMessage) {
-//     console.log(errorMessage);
-//   } else {
-//     console.log(JSON.stringify(results , undefined , 4));
-//   }
-// });
+const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
-//API KEY: 65a85e0bd92a1d7d24e1e5c7d2c8fc22
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  }).help()
+  .alias('help', 'h')
+  .argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  "use strict";
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(results.address);
+    weather.getWeather(results.latitude, results.longitude, (errorMessage, results) => {
+      "use strict";
+      if (errorMessage) {
+        console.log(errorMessage);
+      } else {
+        console.log(`It is currently ${results.temperature}, but it feels like ${results.actualTemperature}`);
+      }
+    });
+  }
+});
--- weather/weather.js
+++ weather/weather.js
@@ -1,15 +1,26 @@
 const request = require('request');
-request({
-    url: 'https://api.darksky.net/forecast/65a85e0bd92a1d7d24e1e5c7d2c8fc22/37.1744876,-93.2586378',
-    json: true
-  }, (error, response, body) => {
-    "use strict";
-    if (error) {
-      console.log('Could not connect to Forecast.io');
-    } else if (response.statusCode === 400) {
-      console.log('Unable to fetch weather');
-    } else if (response.stableStringify === 200) {
-      console.log(body.currently.temperature);
+
+const getWeather = (latitude, longitude, callback) => {
+  "use strict";
+  request({
+      url: `https://api.darksky.net/forecast/65a85e0bd92a1d7d24e1e5c7d2c8fc22/${latitude},${longitude}`,
+      json: true
+    }, (error, response, body) => {
+      "use strict";
+      if (error) {
+        callback('Could not connect to Forecast.io');
+      } else if (response.statusCode === 400) {
+        callback('Unable to fetch weather');
+      } else if (response.statusCode === 200) {
+        callback(undefined, {
+          temperature: body.currently.temperature,
+          actualTemperature: body.currently.apparentTemperature
+        });
+      }
     }
-  }
-);
+  );
+};
+
+module.exports = {
+  getWeather
+};
